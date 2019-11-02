9f0e0f155de71ee0d2ed5e18f35bde47dff562d2
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-01 21:40:09 -0500

    Update models.py

--- discord/models.py
+++ discord/models.py
@@ -1,1 +1,11 @@
-# I
+class BaseChannel:
+    __slots__ = []
+    
+    def __str__(self):
+        return self.name
+
+class BaseUser:
+    __slots__ = []
+    
+    def __str__(self):
+        return self.name
