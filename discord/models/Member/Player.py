1c15b25e02484d088f538e445c0e861ad4e37cd1
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-21 08:23:45 -0600

    Fixed the Player class

--- discord/models/Member/Player.py
+++ discord/models/Member/Player.py
@@ -1,15 +1,15 @@
 from .User import User
-from ..PrizmCls import *
+from .. import PrizmCls
 from ..Raw import RawObjs
 from ..Role import Role
 
-class Player(Member):
+class Player:
     """
     Represents a player [or member] in a guild
     """
     def __init__(self, deaf: bool, hoisted_role: str, joined: str,
                  mute: bool, nick, premium_since: str, roles: list,
-                 user: dict):
+                 user: dict, bot_obj):
         self.user = super().__init__(**user)
         self.vc = {
             "deaf": deaf,
@@ -18,4 +18,5 @@ class Player(Member):
         self.nick = nick
         self.boosted_at: premium_since
 
-        self.roles = RawObjs(Role, roles)
+        self.roles = RawObjs(Role, roles, bot_obj = bot_obj)
+        self.bot_obj = bot_obj
