3ef4a3200adb87248c55cedf24c822ffcc6c3885
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-24 17:46:52 -0600

    Updated package data

--- setup.py
+++ setup.py
@@ -1,34 +1,49 @@
 import setuptools
+import os
 
-with open("README.md", "r") as readme:
-    ext_desc = readme.read()
+with open("README.md", "r") as r:
+    ext_desc = r.read()
     
 with open("req.txt", "r") as r:
     req = r.read().splitlines()
 
+base_url = "https://github.com/VoxelPrismatic/prizmatic"
 setuptools.setup(
+    #Info
     name = "prizmatic",
     version = "0.1.3.6",
     author = "PRIZ ;]",
-    project_urls={
-        "Issue tracker": "https://github.com/VoxelPrismatic/prizmatic/issues",
+    python_requires = ">=3.6",
+    
+    #URLs
+    url = base_url,
+    project_urls = {
+        "Issue tracker": base_url+"/issues",
+        "Source Code": base_url
     },
+    
+    #Description
     description = "An API Wrapper for Reddit, Discord, and Matrix [seperate modules, one repo]",
     long_description = ext_desc,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/VoxelPrismatic/prizmatic",
-    packages = ['discord'],
+    
+    #Packages
+    packages = [
+        'discord'
+    ],
     install_requires = req,
+    
+    #Metadata
     license = "Don't call it your own",
+    keywords = "discord reddit matrix riot", 
     classifiers = [
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: None",
         "Operating System :: OS Dependent",
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-    ],
-    python_requires = '>=3.6'
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ]
 )
