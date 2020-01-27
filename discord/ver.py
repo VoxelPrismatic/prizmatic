70012a5f07001e0db35458f0ee8ab24737c88cad
Priz <danielpetrov@outlook.com>
2020-01-26 21:11:00 -0600

    Updated version parser

--- discord/ver.py
+++ discord/ver.py
@@ -1,6 +1,17 @@
-__ver__ = "0.1.7.6a"
+gens = {
+    "a": "alpha",
+    "b": "beta",
+    "d": "pre-alpha",
+    "z": "pre-pre-alpha"
+}
+
+__ver__ = "0.1.7.6z"
 #major.minor.bugfix.smolfix
-__gen__ = "alpha"
+
+__gen__ = "release"
+if __ver__[-1] in gens:
+    __gen__ = gens[__ver__[-1]]
+
 __ls__ = []
 for c in __ver__:
     if c != ".":
